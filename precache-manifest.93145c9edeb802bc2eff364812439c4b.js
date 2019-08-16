654999ce79a4735c49c6df33b43a90607be1ccc8
Shailesh Kumar Sundram <shailesh.sundram@gmail.com>
2019-08-16 16:52:43 +0530

    Updates

---
+++ precache-manifest.93145c9edeb802bc2eff364812439c4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dab2c7b29c04d257a4a1ea0f972b659",
+    "url": "/sksundram/pics-grid/index.html"
+  },
+  {
+    "revision": "269a94486a2dad46c61e",
+    "url": "/sksundram/pics-grid/static/css/main.53b332df.chunk.css"
+  },
+  {
+    "revision": "2254ed0ee4087936a607",
+    "url": "/sksundram/pics-grid/static/js/2.b41f0cf2.chunk.js"
+  },
+  {
+    "revision": "269a94486a2dad46c61e",
+    "url": "/sksundram/pics-grid/static/js/main.a8cd2a09.chunk.js"
+  },
+  {
+    "revision": "dd285f67de5a821f563a",
+    "url": "/sksundram/pics-grid/static/js/runtime~main.3be18ee9.js"
+  }
+]);
